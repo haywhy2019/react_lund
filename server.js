f5934d09d33813687c2f0aa5003a91458d24d68e
flint <olusegun.sola19@gmail.com>
2020-01-30 15:37:44 +0100

    Update

--- server.js
+++ server.js
@@ -75,10 +75,11 @@ useUnifiedTopology: true
 //serve static asset if in production
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
+  // app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
   })
 }
 
